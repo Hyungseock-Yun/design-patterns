0da04e82a64bde80e2b0a8fc933c1287b3317827
Hyungseock-Yun <bagzein@gmail.com>
2023-05-25 23:01:25 +0900

    [ 싱글톤 패턴 ] - 안전하고 단순하게 구현하는 방법

---
+++ src/main/java/gof/designpatterns/singleton/multithreadsafe/simple/App.java
@@ -0,0 +1,32 @@
+package gof.designpatterns.singleton.multithreadsafe.simple;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class App {
+  public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
+    Settings settings = Settings.INSTANCE;
+//    Settings settings1 = null;
+//
+//    Constructor<?>[] declaredConstructors = Settings.class.getDeclaredConstructors();
+//    for (Constructor<?> constructor : declaredConstructors) {
+//      constructor.setAccessible(true);
+//      settings1 = (Settings) constructor.newInstance("INSTANCE");
+//    }
+//
+//    System.out.println(settings != settings1);
+
+    Settings settings2 = null;
+
+    try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+      out.writeObject(settings);
+    }
+
+    try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+      settings2 = (Settings) in.readObject();
+    }
+
+    System.out.println(settings == settings2);
+  }
+}
---
+++ src/main/java/gof/designpatterns/singleton/multithreadsafe/simple/Settings.java
@@ -0,0 +1,14 @@
+package gof.designpatterns.singleton.multithreadsafe.simple;
+
+/**
+ * enum으로 만들면 리플렉션으로 절대 뚫을 수 없다.
+ * enum은 리플렉션을 막아놓음.
+ * enum은 Serializable을 상속받고 있다.
+ * ( Exception in thread "main" java.lang.IllegalArgumentException: Cannot reflectively create enum objects )
+ * 단점 : 1. 미리 만들어 놓는다. 2. 상속을 사용하지 못한다.
+ */
+public enum Settings {
+
+  INSTANCE
+
+}

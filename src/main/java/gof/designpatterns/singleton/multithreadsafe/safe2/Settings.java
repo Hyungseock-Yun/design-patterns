e3deef74d43557f395da19bbd2e5c9cdfedd1664
Hyungseock-Yun <bagzein@gmail.com>
2023-05-25 22:12:42 +0900

    [ 싱글톤 패턴 ] - 멀티 쓰레드 환경에서 안전하게 구현하는 방법

--- src/main/java/gof/designpatterns/singleton/multithreadsafe/safe2/Settings.java
+++ src/main/java/gof/designpatterns/singleton/multithreadsafe/safe2/Settings.java
@@ -2,7 +2,7 @@ package gof.designpatterns.singleton.multithreadsafe.safe2;
 
 public class Settings {
 
-  private static final Settings INSTANCE = new Settings();
+  private static volatile Settings instance;
 
   private Settings() {}
 
@@ -10,7 +10,19 @@ public class Settings {
    * 이른 초기화(eager initialization) 사용
    */
   public static  Settings getInstance() {
-    return INSTANCE;
+    /**
+     * double checked locking 사용
+     * 인스턴스가 있는 경우에는 synchronized를 거치지 않으므로 성능에 더욱 유리
+     */
+    if (instance == null) {
+      synchronized (Settings.class) {
+        if (instance == null) {
+          instance = new Settings();
+        }
+      }
+    }
+
+    return instance;
   }
 
 }

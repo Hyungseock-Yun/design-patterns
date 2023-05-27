28a858985266cd903f209791d383c779021ccc5e
hsyoon <bagzein@gmail.com>
2023-05-27 16:07:45 +0900

    [ 추상 팩토리 패턴 ] - 패턴 적용 전

---
+++ src/main/java/gof/designpatterns/_01_creational/_03_abstract_factory/_01_before/WhiteWheel.java
@@ -0,0 +1,8 @@
+package gof.designpatterns._01_creational._03_abstract_factory._01_before;
+
+//import me.whiteship.designpatterns._01_creational_patterns._03_abstract_factory._02_after.Wheel;
+
+import gof.designpatterns._01_creational._03_abstract_factory._02_after.Wheel;
+
+public class WhiteWheel implements Wheel {
+}

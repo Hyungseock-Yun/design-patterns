9608644c343979b0034964ce90101b4e0b06f742
Hyungseock-Yun <bagzein@gmail.com>
2023-05-29 22:39:32 +0900

    [ 빌더 패턴 ] - 패턴 적용 후

---
+++ src/main/java/gof/designpatterns/_01_creational/_04_builder/_02_after/App.java
@@ -0,0 +1,27 @@
+package gof.designpatterns._01_creational._04_builder._02_after;
+
+import gof.designpatterns._01_creational._04_builder._01_before.TourPlan;
+
+import java.time.LocalDate;
+
+public class App {
+
+    public static void main(String[] args) {
+//        TourPlanBuilder builder = new DefaultTourBuilder();
+//        builder
+//            .title("칸쿤 여행")
+//            .nightsAndDays(2, 3)
+//            .startDate(LocalDate.of(2020, 12, 9))
+//            .whereToStay("리조트")
+//            .addPlan(0, "체크인하고 짐 풀기")
+//            .addPlan(0, "저녁식사")
+//            .getPlan();
+//
+//        TourPlan longBeachTrip = builder.title("롱비치")
+//                .startDate(LocalDate.of(2021, 7, 15))
+//                .getPlan();
+        TourDirector director = new TourDirector(new DefaultTourBuilder());
+        TourPlan tourPlan = director.cancunTrip();
+        TourPlan tourPlan1 = director.longBeachTrip();
+    }
+}

2b99f5afc7dc0ff7a696dc5aa59d70a86d57dfa1
Hyungseock-Yun <bagzein@gmail.com>
2023-06-03 02:59:54 +0900

    [ 브릿지 패턴 ] - 패턴 적용 후

---
+++ src/main/java/gof/designpatterns/_02_structural/_02_bridge/_02_after/KDA.java
@@ -0,0 +1,9 @@
+package gof.designpatterns._02_structural._02_bridge._02_after;
+
+public class KDA implements Skin {
+
+  @Override
+  public String getName() {
+    return "KDA";
+  }
+}
---
+++ src/main/java/gof/designpatterns/_02_structural/_02_bridge/_02_after/Skin.java
@@ -0,0 +1,5 @@
+package gof.designpatterns._02_structural._02_bridge._02_after;
+
+public interface Skin {
+  String getName();
+}

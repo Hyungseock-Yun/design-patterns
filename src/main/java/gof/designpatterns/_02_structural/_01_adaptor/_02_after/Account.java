d64c62a88422293666461d6ad712d26aa17cc2af
Hyungseock-Yun <bagzein@gmail.com>
2023-06-02 23:36:30 +0900

    [ 어댑터 패턴 ] - 주석 추가

--- src/main/java/gof/designpatterns/_02_structural/_01_adaptor/_02_after/Account.java
+++ src/main/java/gof/designpatterns/_02_structural/_01_adaptor/_02_after/Account.java
@@ -3,6 +3,10 @@ package gof.designpatterns._02_structural._01_adaptor._02_after;
 import gof.designpatterns._02_structural._01_adaptor._02_after.security.UserDetails;
 import lombok.Data;
 
+/**
+ * 클래스를 나누지 않고, 기존 클래스에 직접 implements 해서 사용해도 된다.
+ * (단, SRP 입장에서 보자면 클래스를 쪼개는게 나음)
+ */
 @Data
 public class Account implements UserDetails {
 
--- src/main/java/gof/designpatterns/_02_structural/_01_adaptor/_02_after/AccountService.java
+++ src/main/java/gof/designpatterns/_02_structural/_01_adaptor/_02_after/AccountService.java
@@ -3,6 +3,10 @@ package gof.designpatterns._02_structural._01_adaptor._02_after;
 import gof.designpatterns._02_structural._01_adaptor._02_after.security.UserDetails;
 import gof.designpatterns._02_structural._01_adaptor._02_after.security.UserDetailsService;
 
+/**
+ * 클래스를 나누지 않고, 기존 클래스에 직접 implements 해서 사용해도 된다.
+ * (단, SRP 입장에서 보자면 클래스를 쪼개는게 나음)
+ */
 public class AccountService implements UserDetailsService {
 
   Account findAccountByUsername(String username) {

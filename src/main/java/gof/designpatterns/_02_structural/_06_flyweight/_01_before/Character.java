9dec938dea4e8eec23dc6fbc36860df5009edc42
Hyungseock-Yun <bagzein@gmail.com>
2023-06-05 03:19:38 +0900

    [ 플라이웨이트 패턴 ] - 패턴 적용 전

---
+++ src/main/java/gof/designpatterns/_02_structural/_06_flyweight/_01_before/Character.java
@@ -0,0 +1,16 @@
+package gof.designpatterns._02_structural._06_flyweight._01_before;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Character {
+
+  private char value;
+
+  private String color;
+
+  private String fontFamily;
+
+  private int fontSize;
+
+}

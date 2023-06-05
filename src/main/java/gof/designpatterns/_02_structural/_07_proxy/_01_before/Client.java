462e6532f05742ab3cd1f528652907f7e4c8d677
hsyoon <bagzein@gmail.com>
2023-06-05 15:48:48 +0900

    [ 프록시 패턴 ] - 의존성 변경 - 상속을 사용한 프록시 패턴

--- src/main/java/gof/designpatterns/_02_structural/_07_proxy/_01_before/Client.java
+++ src/main/java/gof/designpatterns/_02_structural/_07_proxy/_01_before/Client.java
@@ -1,9 +1,12 @@
 package gof.designpatterns._02_structural._07_proxy._01_before;
 
+import gof.designpatterns._02_structural._07_proxy._02_after.GameServiceProxyInheritance;
+
 public class Client {
 
   public static void main(String[] args) {
     GameService gameService = new GameService();
+    GameService gameServiceProxy = new GameServiceProxyInheritance();
     gameService.startGame();
   }
 }

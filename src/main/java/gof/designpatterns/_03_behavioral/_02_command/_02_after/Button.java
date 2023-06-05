7257e50bc64d9d17bc50c2431d72571c1bd3fca3
hsyoon <bagzein@gmail.com>
2023-06-05 18:58:48 +0900

    [ 커맨드 패턴 ] - 패턴 추가 적용

--- src/main/java/gof/designpatterns/_03_behavioral/_02_command/_02_after/Button.java
+++ src/main/java/gof/designpatterns/_03_behavioral/_02_command/_02_after/Button.java
@@ -3,22 +3,30 @@ package gof.designpatterns._03_behavioral._02_command._02_after;
 import gof.designpatterns._03_behavioral._02_command._01_before.Game;
 import gof.designpatterns._03_behavioral._02_command._01_before.Light;
 
+import java.util.Stack;
+
 public class Button {
 
-  private Command command;
+  private Stack<Command> commands = new Stack<>();
 
-  public Button(Command command) {
-    this.command = command;
+  public void press(Command command) {
+    command.execute();
+    commands.push(command);
   }
 
-  public void press() {
-    command.execute();
+  public void undo() {
+    if (!commands.isEmpty()) {
+      Command command = commands.pop();
+      command.undo();
+    }
   }
 
   public static void main(String[] args) {
-    Button button = new Button(new GameStartCommand(new Game()));
+    Button button = new Button();
 
-    button.press();
-    button.press();
+    button.press(new GameStartCommand(new Game()));
+    button.press(new LightOnCommand(new Light()));
+    button.undo();
+    button.undo();
   }
 }

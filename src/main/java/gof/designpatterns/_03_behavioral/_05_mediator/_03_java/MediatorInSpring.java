43165dbabeba2aa0ca3bd5f8c54a83171077e336
Hyungseock-Yun <bagzein@gmail.com>
2023-06-07 05:09:34 +0900

    [ 중재자 패턴 ] - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/gof/designpatterns/_03_behavioral/_05_mediator/_03_java/MediatorInSpring.java
@@ -0,0 +1,10 @@
+package gof.designpatterns._03_behavioral._05_mediator._03_java;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MediatorInSpring {
+  public static void main(String[] args) {
+
+    DispatcherServlet dispatcherServlet;
+  }
+}

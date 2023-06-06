089e766fdfee21dd2948a0d3e4d5f787c2b239e3
Hyungseock-Yun <bagzein@gmail.com>
2023-06-06 20:14:18 +0900

    [ 인터프리터 패턴 ] - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/gof/designpatterns/_03_behavioral/_03_interpreter/_03_java/Book.java
@@ -0,0 +1,11 @@
+package gof.designpatterns._03_behavioral._03_interpreter._03_java;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Book {
+
+  private String title;
+}

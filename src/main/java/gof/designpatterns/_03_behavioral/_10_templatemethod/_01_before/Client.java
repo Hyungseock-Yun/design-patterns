90ebb3ffc81d67e89fb7eda7707688f810241258
hsyoon <bagzein@gmail.com>
2023-06-10 13:53:38 +0900

    [ 템플릿 메소드 패턴 ] - 패턴 적용 전

---
+++ src/main/java/gof/designpatterns/_03_behavioral/_10_templatemethod/_01_before/Client.java
@@ -0,0 +1,11 @@
+package gof.designpatterns._03_behavioral._10_templatemethod._01_before;
+
+public class Client {
+
+  public static void main(String[] args) {
+    FileProcessor fileProcessor = new FileProcessor("numbers.txt");
+    int process = fileProcessor.process();
+
+    System.out.println("process = " + process);
+  }
+}

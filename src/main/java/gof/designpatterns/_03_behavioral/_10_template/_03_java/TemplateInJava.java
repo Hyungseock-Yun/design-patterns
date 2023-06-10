8c9837fc85f6c4b3433546ae8f88e70833d8124a
hsyoon <bagzein@gmail.com>
2023-06-10 14:41:11 +0900

    [ 템플릿 메소드 패턴 ] - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/gof/designpatterns/_03_behavioral/_10_template/_03_java/TemplateInJava.java
@@ -0,0 +1,10 @@
+package gof.designpatterns._03_behavioral._10_template._03_java;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class TemplateInJava {
+
+  public static void main(String[] args) {
+    HttpServlet httpServlet;
+  }
+}
